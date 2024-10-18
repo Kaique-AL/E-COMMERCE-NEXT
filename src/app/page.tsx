8f6ee0e819d84142179e37a40dc89a4ec3dee8c8
Kaique.xz <Kaiquebbbb@gmail.com>
2024-10-18 16:11:23 -0300

    DEFININDO TIPAGEM PARA PRODUTO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
+import { ProductType } from "@/types/ProductType"
+async function getProducts() {
+  const res = await fetch ('https://fakestoreapi.com/products')
+
+  if(!res.ok) {
+
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+  
+}
 
 export default async function Home() {
+
+  const products = await getProducts()
+  console.log(products)
+
   return (
-    <div>
-      
+    <div className="max-w-7xl max-auto pt-8 px-8 xl:px-8">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
+      lg:grid-cols-4 gap-10 xl:gap-6">
+        
+        {products.map((product: ProductType) => (
+          <div key={product.id}>
+            {product.title}
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
