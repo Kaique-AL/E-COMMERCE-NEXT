9c7c90cc5b3ec4cec6c7c53709fea413bd9ba16a
Kaique.xz <Kaiquebbbb@gmail.com>
2024-10-26 09:00:22 -0300

    AUTENTICAÇÃO DE USUARIO COM CLERK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import {Inter} from 'next/font/google'
 import clsx from 'clsx';
 import Navbar from './components/NavBar';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ptBR } from '@clerk/localizations';
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,12 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider localization={ptBR}>
       <html lang='en'>
         <body className={clsx(inter.className, 'bg-slate-700')}>
             <Navbar />
-            <main className='h-screen p-16'>{children}</main>
+            <main className='h-screen p-16 flex justify-center'>{children}</main>
           
         </body>
       </html>
+    </ClerkProvider>
   );
 }
